590857fd7df5d5f16f59da7b189494ba26452e06
p2made <pedrofp@me.com>
2021-09-05 00:08:10 +1000

    New Age

--- php/new-age/index.php
+++ php/new-age/index.php
@@ -16,7 +16,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet">
 	<!-- Core theme CSS (includes Bootstrap)-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" rel="stylesheet">
-	<link href="assets/css/new-age.css" rel="stylesheet">
+	<link href="assets/css/styles.css" rel="stylesheet">
 </head>
 <body id="page-top">
 	<!-- Navigation-->
@@ -292,7 +292,7 @@
 	<!-- Bootstrap core JS-->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 	<!-- Core theme JS-->
-	<script src="assets/js/new-age.js"></script>
+	<script src="assets/js/scripts.js"></script>
 	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
 	<!-- * *							   SB Forms JS							   * *-->
 	<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
