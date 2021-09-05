2b1ac8b869befb6a644a5a8018352ed35d8bb5ae
p2made <pedrofp@me.com>
2021-09-05 16:00:37 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,470 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Start Bootstrap Template</title>
+	<!-- Favicon-->
+	<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+	<!-- Core theme CSS (includes Bootstrap)-->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<link href="assets/css/bare.css" rel="stylesheet">
+</head>
+<body>
+	<!-- Responsive navbar-->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="#">Start Bootstrap Themes</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+					<!--
+					<li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+						<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+							<li><a class="dropdown-item" href="#">Action</a></li>
+							<li><a class="dropdown-item" href="#">Another action</a></li>
+							<li><hr class="dropdown-divider"></li>
+							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</li>
+					-->
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<!-- Page content-->
+	<div class="container">
+
+		<div class="row justify-content-center">
+			<!-- Intermediate Layouts -->
+			<div class='col-lg-4'>
+				<h2>Intermediate Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>Agency</th>
+							<td><a href='html/agency/index.html' target='_blank'>html</a></td>
+							<td><a href='php/agency/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Coming Soon</th>
+							<td><a href='html/coming-soon/index.html' target='_blank'>html</a></td>
+							<td><a href='php/coming-soon/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Creative</th>
+							<td><a href='html/creative/index.html' target='_blank'>html</a></td>
+							<td><a href='php/creative/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Freelancer</th>
+							<td><a href='html/freelancer/index.html' target='_blank'>html</a></td>
+							<td><a href='php/freelancer/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Grayscale</th>
+							<td><a href='html/grayscale/index.html' target='_blank'>html</a></td>
+							<td><a href='php/grayscale/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Landing Page</th>
+							<td><a href='html/landing-page/index.html' target='_blank'>html</a></td>
+							<td><a href='php/landing-page/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>New Age</th>
+							<td><a href='html/new-age/index.html' target='_blank'>html</a></td>
+							<td><a href='php/new-age/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>One Page Wonder</th>
+							<td><a href='html/one-page-wonder/index.html' target='_blank'>html</a></td>
+							<td><a href='php/one-page-wonder/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Resume</th>
+							<td><a href='html/resume/index.html' target='_blank'>html</a></td>
+							<td><a href='php/resume/index.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!-- Complex Layouts -->
+			<div class='col-lg-4'>
+				<h2>Complex Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>Business Casual</th>
+							<td><a href='html/business-casual/index.html' target='_blank'>html</a></td>
+							<td><a href='php/business-casual/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Clean Blog</th>
+							<td><a href='html/clean-blog/index.html' target='_blank'>html</a></td>
+							<td><a href='php/clean-blog/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Modern Business</th>
+							<td><a href='html/modern-business/index.html' target='_blank'>html</a></td>
+							<td><a href='php/modern-business/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>SB Admin</th>
+							<td><a href='html/sb-admin/index.html' target='_blank'>html</a></td>
+							<td><a href='php/sb-admin/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>SB Admin 2</th>
+							<td><a href='html/sb-admin-2/index.html' target='_blank'>html</a></td>
+							<td><a href='php/sb-admin-2/index.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!-- Layouts -->
+			<div class='col-lg-4'>
+				<h2>Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<!-- Basic Layouts -->
+			<div class='col-lg-4'>
+				<h2>Basic Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>Bare</th>
+							<td><a href='html/basic-layouts/bare.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/bare.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Blog Home</th>
+							<td><a href='html/basic-layouts/blog-home.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/blog-home.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Blog Post</th>
+							<td><a href='html/basic-layouts/blog-post.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/blog-post.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Business Frontpage</th>
+							<td><a href='html/basic-layouts/business-frontpage.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/business-frontpage.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Full Width Pics</th>
+							<td><a href='html/basic-layouts/full-width-pics.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/full-width-pics.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Heroic Features</th>
+							<td><a href='html/basic-layouts/heroic-features.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/heroic-features.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Scrolling Nav</th>
+							<td><a href='html/basic-layouts/scrolling-nav.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/scrolling-nav.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Shop Homepage</th>
+							<td><a href='html/basic-layouts/shop-homepage.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/shop-homepage.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Shop Item</th>
+							<td><a href='html/basic-layouts/shop-item.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/shop-item.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Simple Sidebar</th>
+							<td><a href='html/basic-layouts/simple-sidebar.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/simple-sidebar.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Small Business</th>
+							<td><a href='html/basic-layouts/small-business.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/small-business.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>The Big Picture</th>
+							<td><a href='html/basic-layouts/the-big-picture.html' target='_blank'>html</a></td>
+							<td><a href='php/basic-layouts/the-big-picture.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!-- Snippet Layouts -->
+			<div class='col-lg-4'>
+				<h2>Snippet Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>Registration Page With Floating Labels</th>
+							<td><a href='html/snippets/registration-page-with-floating-labels.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/registration-page-with-floating-labels.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Responsive Pricing Table</th>
+							<td><a href='html/snippets/responsive-pricing-table.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/responsive-pricing-table.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Video Background Header</th>
+							<td><a href='html/snippets/video-background-header.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/video-background-header.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!-- Snippet Layouts (checked) -->
+			<div class='col-lg-4'>
+				<h2>Snippet Layouts (checked)</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>About and Team Section</th>
+							<td><a href='html/snippets/about-and-team-section.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/about-and-team-section.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Contact Form Split Image Layout</th>
+							<td><a href='html/snippets/contact-form-split-image-layout.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/contact-form-split-image-layout.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Contact Form With Validation</th>
+							<td><a href='html/snippets/contact-form-with-validation.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/contact-form-with-validation.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Dropdown Menu Animation Navbar</th>
+							<td><a href='html/snippets/dropdown-menu-animation-navbar.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/dropdown-menu-animation-navbar.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Flexbox Sticky Footer</th>
+							<td><a href='html/snippets/flexbox-sticky-footer.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/flexbox-sticky-footer.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Full Page Image Header</th>
+							<td><a href='html/snippets/full-page-image-header.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/full-page-image-header.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Full Page Image HTML Background</th>
+							<td><a href='html/snippets/full-page-image-html-background.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/full-page-image-html-background.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Full Page Image Slider Header</th>
+							<td><a href='html/snippets/full-page-image-slider-header.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/full-page-image-slider-header.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Half Page Image Slider Header</th>
+							<td><a href='html/snippets/half-page-image-slider-header.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/half-page-image-slider-header.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Image Gallery With Thumbnails</th>
+							<td><a href='html/snippets/image-gallery-with-thumbnails.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/image-gallery-with-thumbnails.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Login Form With Floating Labels</th>
+							<td><a href='html/snippets/login-form-with-floating-labels.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/login-form-with-floating-labels.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Modern Sign in Page With Split Screen Format</th>
+							<td><a href='html/snippets/modern-sign-in-page-with-split-screen-format.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/modern-sign-in-page-with-split-screen-format.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Navbar With Logo Image</th>
+							<td><a href='html/snippets/navbar-with-logo-image.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/navbar-with-logo-image.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>Portfolio Item Details Page</th>
+							<td><a href='html/snippets/portfolio-item-details-page.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/portfolio-item-details-page.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<!-- Portfolio Layouts -->
+			<div class='col-lg-4'>
+				<h2>Portfolio Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>Stylish Portfolio</th>
+							<td><a href='html/stylish-portfolio/index.html' target='_blank'>html</a></td>
+							<td><a href='php/stylish-portfolio/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>1 Column Portfolio Layout</th>
+							<td><a href='html/snippets/1-column-portfolio-layout.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/1-column-portfolio-layout.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>2 Column Portfolio Layout</th>
+							<td><a href='html/snippets/2-column-portfolio-layout.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/2-column-portfolio-layout.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>3 Column Portfolio Layout</th>
+							<td><a href='html/snippets/3-column-portfolio-layout.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/3-column-portfolio-layout.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>4 Column Portfolio Layout</th>
+							<td><a href='html/snippets/4-column-portfolio-layout.html' target='_blank'>html</a></td>
+							<td><a href='php/snippets/4-column-portfolio-layout.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!-- Layouts -->
+			<div class='col-lg-4'>
+				<h2>Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!-- Layouts -->
+			<div class='col-lg-4'>
+				<h2>Layouts</h2>
+				<table class='table'>
+					<tbody>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+						<tr>
+							<th scope='row'>_layout_name_</th>
+							<td><a href='html/_layout_name_/index.html' target='_blank'>html</a></td>
+							<td><a href='php/_layout_name_/index.php' target='_blank'>php</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+	</div>
+
+<!--
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				Column
+			</div>
+			<div class="col-lg-4">
+				Column
+			</div>
+			<div class="col-lg-4">
+				Column
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-2">
+				Column
+			</div>
+			<div class="col-lg-1">
+				Column
+			</div>
+			<div class="col-lg-1">
+				Column
+			</div>
+			<div class="col-lg-2">
+				Column
+			</div>
+			<div class="col-lg-1">
+				Column
+			</div>
+			<div class="col-lg-1">
+				Column
+			</div>
+			<div class="col-lg-2">
+				Column
+			</div>
+			<div class="col-lg-1">
+				Column
+			</div>
+			<div class="col-lg-1">
+				Column
+			</div>
+		</div>
+
+		<div class='col-lg-4'>
+			<h2>New Layouts</h2>
+			<table class='table'>
+				<tbody>
+				</tbody>
+			</table>
+		</div>
+	</div>
+  -->
+
+	<!-- Bootstrap core JS-->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+	<!-- Core theme JS-->
+	<script src="p2-startbootstrap/assets/js/bare.js"></script>
+</body>
+</html>
+
+
